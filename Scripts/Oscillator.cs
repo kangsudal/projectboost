c400a29e8a2af0606e5fab7e8138bec8d2bb404c
kangsudal_notebook <kangsudal@gmail.com>
2022-06-28 17:30:01 +0900

    57. Mathf.Sin() for oscillation

--- Scripts/Oscillator.cs
+++ Scripts/Oscillator.cs
@@ -6,7 +6,8 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField][Range(0, 1)] float movementFactor;
+    float movementFactor;
+    [SerializeField] float period = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,13 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period; // continually growing over time
+
+        const float tau = Mathf.PI * 2; // constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1 so its cleaner
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
